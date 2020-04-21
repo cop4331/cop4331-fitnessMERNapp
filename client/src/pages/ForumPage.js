e044d46652c3959d3fd7f8acb4d9639ecd5e882c
Austin <62572417+au7413@users.noreply.github.com>
2020-04-20 22:03:10 -0400

    Update ForumPage.js

--- client/src/pages/ForumPage.js
+++ client/src/pages/ForumPage.js
@@ -79,7 +79,7 @@ class ForumPage extends React.Component {
   displayBlogPost = (posts) => {
     
     
-     if (!Object.keys(posts).length) {
+     if (posts.length) {
        //console.log("error null posts arr");
        return null;
      }
